6837a0a8b5c5179504c98d4192a41fbd3e62337e
MaXoN41k <zabot1n.m@yandex.ru>
2022-11-19 01:13:13 +0300

    class Stack was written

---
+++ Arithmetic/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Stack.h"
+using namespace std;
+
+int main()
+{
+  TStack<int> stop(2);
+  //stop.Push(3);
+  //stop.Push(4);
+  //cout << "size1=" << stop.GetSize() << endl;
+  //stop.Push(5);
+  //cout << "size2=" << stop.GetSize() << endl;
+  //cout << "start2=" << stop.GetStart() << endl;
+  //cout << "elem = " << stop.Top() << endl;
+  //cout << "start3=" << stop.GetStart() << endl;
+  //cout << "elem1 = " << stop.Top() << endl;
+  //cout << "start4=" << stop.GetStart() << endl;
+  //stop.Pop();
+  //cout << "start5=" << stop.GetStart() << endl;
+  //cout << "elem2 = " << stop.Top() << endl;
+  return 0;
+}
